3ca9ae59bdcb26aa526741cb74aa869322fe28ef
Slice Wess <ernestwesson86@gmail.com>
2020-09-19 00:16:28 -0400

    adding some functions

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 
 var generateBtn = document.querySelector("#generate");
-// Used the let to 
+// Used the let command to declare my variable
 let userCharac = [];;
 let userPassword = [];
 
@@ -12,6 +12,29 @@ let numberCharc = "0123456789"
 let upperCharc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 let lowerCharc = "abcdefghijklmnopqrstuvwxyz"
 let specialCharc = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+// Password Function
+
+let length = prompt("Enter the length of your userPassword. *Minimum of 8 characters, the limit is 128 characters");
+console.log(length);
+
+let numbers = confirm("If you want numbers select 'Ok' ")
+if(numbers) {
+    userCharac.push(...numberCharac);
+}
+console.log(numbers)
+
+let upper = confirm();
+if(upper) {
+    userCharac.push(...upperCharac);
+}
+console.log(upper);
+
+let lower = confirm();
+if(lower) {
+    userCharac.push(...lowerCharac);
+    
+}
 
 
 
