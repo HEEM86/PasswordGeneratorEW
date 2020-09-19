55aa6dcf7ccbf12c250a32a04176316fcb2953c4
Slice Wess <ernestwesson86@gmail.com>
2020-09-18 23:38:02 -0400

    finished adding values for the let commands

--- script.js
+++ script.js
@@ -4,7 +4,14 @@
 
 
 var generateBtn = document.querySelector("#generate");
-
+// Used the let to 
 let userCharac = [];;
 let userPassword = [];
+
+let numberCharc = "0123456789"
+let upperCharc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let lowerCharc = "abcdefghijklmnopqrstuvwxyz"
+let specialCharc = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+
 
